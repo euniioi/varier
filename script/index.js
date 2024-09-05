7d9fb7ab87755f9d15d2669e1ed125171d2a036e
euniioi <ji6723@naver.com>
2024-09-05 15:17:32 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 
 $(function () {
     var swiper = new Swiper('.gallery .gallery_inner ', {
-        slidesPerView: 1,//보여지는 갤러리 수
+        slidesPerView: 2,//보여지는 갤러리 수
         spaceBetween: 50,//갤러리 사이 간격
         speed: 2000,//버튼을 슬라이드가 넘어가는 시간
         autoplay: {
